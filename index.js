6d2c39f19a5b6c9f112774911c41884069cfa7d4
Yazuak <yazuak@gmail.com>
2013-12-27 13:54:32 -0600

    Update index.js
    Added proper rotation and calculus-based dot spacing. Kind of a mess.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var max = 19,
+var max = 300, //num dots
+  dotSize = 7,
   xScale = 6,
-  zScale = 2.5,
+  zScale = 6,
   yScale = 16,
   startFrom = 0,
-  dz = 640,
+  theta = Math.PI / 16,
+  yOfXAxis = 300,
+  spinSpeed =  0.005, //Math.PI / 128,
+  fps = 24,
 
   // I actually want it to be slower then 60fps
   requestAnimationFrame = function(callback) {
-      window.setTimeout(callback, 1000 / 24);
+      window.setTimeout(callback, 1000 / fps);
   };
 
 function run() {
@@ -15,25 +19,25 @@ function run() {
     redSpiralShadow = createSpiral({
       foreground: "#660000",
       background: "#330000",
-      isLeft: true,
+      rotation: Math.PI,
       yLocalScale: 1.01
     }),
     redSpiral = createSpiral({
       foreground: "#ff0000",
       background: "#440000",
-      isLeft: true,
+      rotation: Math.PI,
       yLocalScale: 1
     }),
     cyanSpiralShadow = createSpiral({
       foreground: "#003300",
       background: "#000000",
-      isLeft: false,
+      rotation: 0,
       yLocalScale: 1.01
     }),
     cyanSpiral = createSpiral({
       foreground: "#00ffcc",
       background: "#005633",
-      isLeft: false,
+      rotation: 0,
       yLocalScale: 1
     });
 
@@ -52,6 +56,7 @@ function run() {
   }
 
   function renderFrame() {
+    theta += spinSpeed;
     ctx.clearRect(0, 0, 480, 640);
     ctx.beginPath();
 
@@ -65,52 +70,75 @@ function run() {
   }
 
   function forEachStep(callback) {
-    for (var i = -startFrom; i < max + startFrom; i += 0.08) {
-      if (i < 0 || i > max) continue;
-      callback(i);
+    var t = -startFrom;
+    for (var i = -5; i < max; i++) {
+      t = callback(t);
     }
   }
 
   function createSpiral(config) {
-    var sign = config.isLeft ? -1 : 1,
+    var rotation = config.rotation,
       background = config.background,
       foreground = config.foreground,
       yLocalScale = config.yLocalScale || 1;
 
-    if (!config.isLeft) {
-      background = foreground;
-      foreground = config.background;
-    }
+    return function(t) {
+      var dt = dotSize / Math.sqrt(dx(t, rotation)*dx(t, rotation)+dy(t)*dy(t)+dz(t, rotation)*dz(t, rotation));
+      if(t > 0 && t < 26) // to get the exact value we'd need to integrate, a bit beyond the scope of this
+      {
+        //console.log(t + " : " + dt);
+          z = getZ(t, rotation)
+          x = getX(t, rotation),
+          y = getY(t * yLocalScale, z);
 
-    return function(i) {
-      var zoff = i * Math.sin(i),
-        z = dz / (dz - sign * zoff * zScale),
-        x = getX(i, z, sign),
-        y = getY(i * yLocalScale, z);
-
-      if (zoff + sign * Math.PI / 4 < 0) {
-        switchColor(foreground);
-      } else {
-        switchColor(background);
+        if (z < 0) {
+          switchColor(foreground);
+        } else {
+          switchColor(background);
+        }
+        ctx.moveTo(x, y);
+        ctx.lineTo(getX(t + dt / 2, rotation), getY((t + dt / 2) * yLocalScale, getZ(t + dt / 2, rotation)));
       }
-      ctx.moveTo(x, y);
-      ctx.lineTo(getX(i + 0.03, z, sign), getY((i + 0.01) * yLocalScale, z));
+      t += dt;
+      return t;
     };
   }
 
   function switchColor(color) {
-    ctx.closePath();
+    ctx.closePath()
     ctx.stroke();
 
     ctx.strokeStyle = color;
     ctx.beginPath();
   }
 
-  function getX(i, z, sign) {
-    return sign * i * Math.cos(i) * z * xScale + 255;
+  function getX(i, rotation) {
+    return i * Math.cos(i + rotation) * xScale + 255;
   }
 
   function getY(i, z) {
-    return i * z * yScale + 50;
+    var y = i * yScale + 50;
+    var py = (y - yOfXAxis) * Math.cos(theta) - z * Math.sin(theta) + yOfXAxis; //rotate around x axis, which is at z = 0, y = yOfXAxis
+    return py;
+  }
+
+  function getZ(i, rotation) {
+    return i * Math.sin(i + rotation) * zScale;
+  }
+
+  //todo: consolidate these
+  function dx(t, rotation)
+  {
+    return (Math.cos(t + rotation) - t * Math.sin(t + rotation)) * xScale;
+  }
+
+  function dy(t)
+  {
+    return yScale;
+  }
+
+  function dz(t, rotation)
+  {
+    return (Math.sin(t + rotation) + t * Math.cos(t + rotation)) * zScale;
   }
 }
